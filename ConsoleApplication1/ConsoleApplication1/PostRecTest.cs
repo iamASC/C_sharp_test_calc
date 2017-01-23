da46258bf3a14d3b6f9c2bf15ce9b5f8337c6d6a
Sergei Alabugin <sergei_alabugin@mail.ru>
2017-01-23 18:57:11 +0600

    Первый тест для класса постфиксной записи

---
+++ ConsoleApplication1/ConsoleApplication1/PostRecTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class PostRecFirstTest
+    {
+        String input = "1 + 2 * 3";
+        String res = "1 2 3 * + ";
+        PostRec test = new PostRec(input);
+        if (test.rec != res){
+            System.Console.WriteLine("Test Failed");
+        }else{
+            System.Console.WriteLine("Test Passed");
+        }
+           
+
+    }
+}
