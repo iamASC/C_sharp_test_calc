7f440f27e1bae6fb53ac7fa819d1c51bdfee64ed
Sergei Alabugin <sergei_alabugin@mail.ru>
2017-01-24 17:22:02 +0600

    Тест для класса Resolver (этот класс решает выражение)

---
+++ ConsoleApplication1/ConsoleApplication1/ResolverTest.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class ResolverTest
+    {
+        public void Test()
+        {
+            Resolver test = new Resolver();
+            Console.WriteLine("1+2+3={0}", test.solve("1+2+3"));
+        }
+    }
+}
