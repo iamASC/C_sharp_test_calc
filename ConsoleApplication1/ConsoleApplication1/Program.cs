3b491de0e1a5649aa6c7e140566defd92668fcb0
Sergei Alabugin <sergei_alabugin@mail.ru>
2017-01-25 00:54:53 +0600

    Добавлен консольный интерфейс

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            //TESTS
+            /*
             PostRecFirstTest test = new PostRecFirstTest();
             test.Test1();
             test.Test2();
@@ -20,6 +22,14 @@ namespace ConsoleApplication1
 
             ResolverTest test3 = new ResolverTest();
             test3.Test();
+             */
+
+            Console.WriteLine("Please, enter your mathematical expression:");
+            String input = "";
+            input = Console.ReadLine();
+            Resolver res = new Resolver();
+            Console.WriteLine(res.Solve(input));
+        
         }
     }
 }
